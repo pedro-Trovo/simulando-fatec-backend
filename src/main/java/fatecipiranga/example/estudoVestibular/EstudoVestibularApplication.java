a2234f386da5b1e51dce61a9960ffa7827a80dfe
Pedro Trovo <trovopedro5@gmail.com>
2024-11-02 18:45:38 -0300

    feat: adicionar todos os arquivos do projeto

---
+++ src/main/java/fatecipiranga/example/estudoVestibular/EstudoVestibularApplication.java
@@ -0,0 +1,13 @@
+package fatecipiranga.example.estudoVestibular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoVestibularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoVestibularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fatecipiranga/example/estudoVestibular/EstudoVestibularApplicationTests.java
@@ -0,0 +1,13 @@
+package fatecipiranga.example.estudoVestibular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoVestibularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
