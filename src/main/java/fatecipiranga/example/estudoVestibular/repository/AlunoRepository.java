2f11bf0fd7f8eac7e0ef4d3b36937dd33380906b
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-09 20:18:47 -0300

    fix(aluno-controller): função de cadastro agora recebe um optional de aluno

--- src/main/java/fatecipiranga/example/estudoVestibular/repository/AlunoRepository.java
+++ src/main/java/fatecipiranga/example/estudoVestibular/repository/AlunoRepository.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    @Query(value = "SELECT COUNT(*) FROM aluno WHERE email = ?1", nativeQuery = true)
-    Long checarEmailExiste(String email);
-
     @Query(value = "SELECT * FROM aluno WHERE email = ?1", nativeQuery = true)
     Optional<Aluno> procurarLogin(String email);
 }
